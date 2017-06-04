da82fc911da501278f93a39a81154063dd6c7776
Randy Fay <randy@randyfay.com>
2017-06-04 16:00:49 -0600

    Remove depenencies

--- Formula/ddev.rb
+++ Formula/ddev.rb
@@ -8,8 +8,10 @@ class Ddev < Formula
   url "https://github.com/drud/ddev/releases/download/v0.5/ddev_darwin.tar.gz"
   sha256 "294657859ee099fb6284cb3ac2f43165c21bb72285c167fff8474d0d53ed89a9"
 
-  depends_on "docker" => :run
-  depends_on "docker-compose" => :run
+  # Dependencies don't currently seem to be useful since brew doesn't have edge and people likely will not have
+  # used brew to install docker.
+  # depends_on "docker" => :run
+  # depends_on "docker-compose" => :run
 
 
   bottle do
