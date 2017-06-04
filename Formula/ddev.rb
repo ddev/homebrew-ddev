def99a5d82c793c84990d6d0a184cc25a6db02a3
Randy Fay <randy@randyfay.com>
2017-06-04 16:33:28 -0600

    Another try

--- Formula/ddev.rb
+++ Formula/ddev.rb
@@ -17,11 +17,12 @@ class Ddev < Formula
 
 
   bottle do
-    root_url 'http://thefays.us/tmp'
+    root_url "http://thefays.us/tmp"
     sha256 "294657859ee099fb6284cb3ac2f43165c21bb72285c167fff8474d0d53ed89a9" => :sierra
   end
 
   def install do
+    system "tar", "-zxf", "*.tar.gz"
     bin.install "ddev"
   def end
 
