3cc80e7dd5087676457dc7c6dd47cb72d2128caa
Kevin Bridges <kevin@cyberswat.com>
2017-08-23 13:36:05 -0600

    Updating to v0.9.1

--- Formula/ddev.rb
+++ Formula/ddev.rb
@@ -1,8 +1,8 @@
 class Ddev < Formula
   desc "ddev: a local development environment management system"
   homepage "https://ddev.readthedocs.io/en/latest/"
-  url "https://github.com/drud/ddev/releases/download/v0.9/ddev_osx.v0.9.tar.gz"
-  sha256 "7db90286b97c1ac12a548fa656738b23e9172f1cc44fb5ba23d8ed4b2fd8a00c"
+  url "https://github.com/drud/ddev/releases/download/v0.9.1/ddev_osx.v0.9.1.tar.gz"
+  sha256 "b073e0d39f0bb2cbb1684f30b023426853ce577a448c25933ae24b5477678e75"
 
   # Dependencies don't currently seem to be useful since brew doesn't have edge and people likely will not have
   # used brew to install docker.
