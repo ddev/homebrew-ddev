6b25c3d37c3c9d3c01f221739f15c148121a0d5a
Randy Fay <randy@randyfay.com>
2018-03-15 15:40:26 -0600

    Update to v0.15.1

--- Formula/ddev.rb
+++ Formula/ddev.rb
@@ -1,8 +1,8 @@
 class Ddev < Formula
   desc "ddev: a local development environment management system"
   homepage "https://ddev.readthedocs.io/en/latest/"
-  url "https://github.com/drud/ddev/releases/download/v0.15.0/ddev_macos.v0.15.0.tar.gz"
-  sha256 "3800be2cf33e470bc653220ee78a07188d1537c24c87d393e1335c80f6857a3b"
+  url "https://github.com/drud/ddev/releases/download/v0.15.1/ddev_macos.v0.15.1.tar.gz"
+  sha256 "755b394bcdb27f10c3b85bf00c6955cd3ea363b2b96c7c778a3a0a2c2e0ab7ad"
 
   # Dependencies don't currently seem to be useful since people likely will not have
   # used brew to install docker.
