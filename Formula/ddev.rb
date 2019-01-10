a2f0ae7ed5d33960db748284963b74de7c1bc596
Randy Fay <randy@randyfay.com>
2019-01-10 10:04:59 -0700

    Add bottles for linux and mac

--- Formula/ddev.rb
+++ Formula/ddev.rb
@@ -9,6 +9,13 @@ class Ddev < Formula
   depends_on "docker" => :build
   depends_on "go" => :build
 
+  bottle do
+    root_url "https://dl.bintray.com/drud/bottles-ddev"
+    cellar :any_skip_relocation
+    sha256 "16316d98bcad17ba0b53f37bab2b134835f6a6d334ec1485e43cf620ae9a8cf5" => :x86_64_linux
+    sha256 "d1139d57fb8ea0f3c75563caf81ab091e5ba096008b107f1fa15465a067c51ce" => :el_capitan
+  end
+
   def install
     system "make", "VERSION=v#{version}", "COMMIT=v#{version}"
     system "mkdir", "-p", "#{bin}"
