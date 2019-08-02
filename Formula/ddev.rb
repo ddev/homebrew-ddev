e6d9c995002acbc75881496010f7b81bb335480a
Randy Fay <randy@randyfay.com>
2019-08-02 07:15:05 -0600

    Update to v1.10.0 (#39)

--- Formula/ddev.rb
+++ Formula/ddev.rb
@@ -1,8 +1,8 @@
 class Ddev < Formula
   desc "ddev: a local development environment management system"
   homepage "https://ddev.readthedocs.io/en/stable/"
-  url "https://github.com/drud/ddev/archive/v1.9.1.tar.gz"
-  sha256 "c469effc39a606cb37f5f547805a2ac17d2f95a773bc15226b7c60144bad0027"
+  url "https://github.com/drud/ddev/archive/v1.10.0.tar.gz"
+  sha256 "179db8597d6f2e17e6a96976817350150587de1b9616c126255d81f447ba337c"
 
   # depends_on "docker" => :run
   # depends_on "docker-compose" => :run
@@ -12,12 +12,11 @@ class Ddev < Formula
   depends_on "nss" => :run
 
   bottle do
-    root_url "https://github.com/drud/ddev/releases/download/v1.9.1/"
+    root_url "https://github.com/drud/ddev/releases/download/v1.10.0/"
     cellar :any_skip_relocation
-    sha256 "d195bc3df6e05a9a44ff9b19847718a362bf2f5f6091feb7e53dfce22a6af14e" => :x86_64_linux
-    sha256 "394cecbc7653e30e807080f8d4f115df87e40db9b3cdfbfdd2282025a96c8d99" => :sierra
+    sha256 "3fa389dfdbaa65016cec0b9fd5e8f810b5a376bf1e2c7a274b4eb243a1c7b2ad" => :x86_64_linux
+    sha256 "19c4379b995eddff82f7a096774edd075b45199fa3cab5fa6444233e0b5ea1d9" => :sierra
   end
-
   def install
     system "make", "VERSION=v#{version}", "COMMIT=v#{version}"
     system "mkdir", "-p", "#{bin}"
@@ -37,7 +36,7 @@ class Ddev < Formula
 
   def caveats
   <<~EOS
-PLEASE MAKE SURE to do `mkcert -install`, which may require your sudo password.
+Make sure to do a `mkcert -install` if you haven't done it before, it may require your sudo password.
 
 ddev requires docker and docker-compose.
 Docker installation instructions at https://ddev.readthedocs.io/en/stable/users/docker_installation/
