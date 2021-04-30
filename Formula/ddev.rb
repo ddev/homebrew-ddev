385f681a30fafac50e8eefeb57a7d35e77acb0cb
rfay <randy@randyfay.com>
2021-04-30 14:10:16 +0000

    Homebrew bump to v1.17.2

--- Formula/ddev.rb
+++ Formula/ddev.rb
@@ -1,17 +1,17 @@
 class Ddev < Formula
   desc "Local development environment management system"
   homepage "https://ddev.readthedocs.io/en/stable/"
-  url "https://github.com/drud/ddev/archive/v1.17.1.tar.gz"
-  sha256 "6af19c13969189d0d77a5b5aab5bb784f17c2acb87c4b6b380b5a62b63db78f9"
+  url "https://github.com/drud/ddev/archive/v1.17.2.tar.gz"
+  sha256 "2f713184ff05ee8bc0bd8f61bef3c3db7097713b10d3dac7d49cfe9a63e1170e"
 
   depends_on "mkcert" => :run
   depends_on "nss" => :run
 
   bottle do
-    root_url "https://github.com/drud/ddev/releases/download/v1.17.1/"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "d5b53ee613d28f90540bb20a6cc70b457946ff8b64e4aae428d348ea49ffde41"
-    sha256 cellar: :any_skip_relocation, high_sierra: "28bca291599c56bec6b430193c7728ee595623ee5e46536f4430606bdddc2998"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "803a3c50f0ec91ac44ea17e93243f1d8923b0715db1c41e0532d733c7ba1972a"
+    root_url "https://github.com/drud/ddev/releases/download/v1.17.2/"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "43016761c645252dc989612c219c4bc286cb10a5deeaa2a55c8acc7721e287b4"
+    sha256 cellar: :any_skip_relocation, high_sierra: "46d017a1ae06315e64ae3341e63c09a114721e6cd79631438a853bcc445c10c9"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "15a4fe66cba38d0443cad5408b6b5c84052cfec19fac5e9fee747454858dc9c1"
   end
   def install
     system "make", "VERSION=v#{version}", "COMMIT=v#{version}"
